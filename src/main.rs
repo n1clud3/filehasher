1b9dbdaee71f511db8bfaeb3e5f24b47f5a70be4
3pixl <kolyabarabot07@gmail.com>
2022-03-09 14:53:17 +0200

    0.2.0 now using more updated libraries added sha128, sha224, sha384, sha512 hashes

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,14 @@ use clap::Parser;
 
 mod util;
 
-const POSSIBLE_ALGORITHMS: [&'static str; 2] = ["md5", "sha256"];
+const ALGORITHMS: [&str; 6] = ["md5", "sha128", "sha224", "sha256", "sha384", "sha512"];
 
 #[derive(Parser)]
 #[clap(author, version, about, long_about = None)]
 struct Cli {
     #[clap(short, long,
         default_value = "md5",
-        possible_values = POSSIBLE_ALGORITHMS,
+        possible_values = ALGORITHMS,
         help = "Hash algorithm to use"
     )]
     algorithm: String,
@@ -25,8 +25,14 @@ fn main() {
     let content = std::fs::read(&args.path).unwrap();
     let result = match args.algorithm.as_str() { 
         "md5" => util::compute_md5(content),
+        "sha128" => util::compute_sha128(content),
+        "sha224" => util::compute_sha224(content),
         "sha256" => util::compute_sha256(content),
-        _ => panic!("how? unknown algorithm {}", args.algorithm)
+        "sha384" => util::compute_sha384(content),
+        "sha512" => util::compute_sha512(content),
+        
+        // It's impossible to get to this arm. But if you do, please make an issue
+        _ => panic!("unknown algorithm {:?}", args.algorithm)
     };
 
     println!("The {} hash of {:?} is {}", args.algorithm, args.path, result)
--- src/util.rs
+++ src/util.rs
@@ -1,8 +1,51 @@
+use md5::Md5;
+use sha1::Sha1;
+use sha2::{Sha224, Sha256, Sha384, Sha512, Digest};
+
 pub fn compute_md5(input: Vec<u8>) -> String {
-    let digest = md5::compute(input);
-    format!("{:x}", digest)
+    let mut hasher = Md5::new();
+    hasher.update(input);
+    let result = hasher.finalize();
+
+    format!("{:x}", result) // Format the result as string
+}
+
+pub fn compute_sha128(input: Vec<u8>) -> String {
+    let mut hasher = Sha1::new();
+    hasher.update(input);
+    let result = hasher.finalize();
+
+    format!("{:x}", result) // Format the result as string
+}
+
+pub fn compute_sha224(input: Vec<u8>) -> String {
+    let mut hasher = Sha224::new();
+    hasher.update(input);
+    let result = hasher.finalize();
+
+    format!("{:x}", result) // Format the result as string
 }
 
 pub fn compute_sha256(input: Vec<u8>) -> String {
-    sha256::digest_bytes(&input)
+    let mut hasher = Sha256::new();
+    hasher.update(input);
+    let result = hasher.finalize();
+
+    format!("{:x}", result) // Format the result as string
+}
+
+pub fn compute_sha384(input: Vec<u8>) -> String {
+    let mut hasher = Sha384::new();
+    hasher.update(input);
+    let result = hasher.finalize();
+
+    format!("{:x}", result) // Format the result as string
+}
+
+pub fn compute_sha512(input: Vec<u8>) -> String {
+    let mut hasher = Sha512::new();
+    hasher.update(input);
+    let result = hasher.finalize();
+
+    format!("{:x}", result) // Format the result as string
 }
