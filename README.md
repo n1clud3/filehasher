7b2151793e9c065770070b02f2f179f68093b59d
n1clud3 <kolyabarabot07@gmail.com>
2022-03-09 17:06:21 +0200

    0.3.0 implemented error handling when reading PathBuf of inexistent file implemented unformatted flag to print raw digest updated demonstration gif

--- README.md
+++ README.md
@@ -12,5 +12,8 @@ CLI utility, which outputs hash of the given file.
 
 ### Options
 
-`-a, --algorithm` - hash algorithm to use.
-`md5` by default. For possible values, please see `filehasher --help`
+`-a, --algorithm` - hash algorithm to use. `md5` by default.
+For possible values, please see `filehasher --help`
+
+`-u, --unformatted` - use it to print raw digest instead of pretty line.
+Useful for pipe operations idk.
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
-#![allow(unused)]
-
 use clap::Parser;
+use colored::Colorize;
 
 mod util;
 
@@ -9,31 +8,45 @@ const ALGORITHMS: [&str; 6] = ["md5", "sha128", "sha224", "sha256", "sha384", "s
 #[derive(Parser)]
 #[clap(author, version, about, long_about = None)]
 struct Cli {
-    #[clap(short, long,
-        default_value = "md5",
-        possible_values = ALGORITHMS,
-        help = "Hash algorithm to use"
-    )]
+    #[clap(short, long, default_value = "md5", possible_values = ALGORITHMS, help = "Hash algorithm to use")]
     algorithm: String,
     #[clap(parse(from_os_str), help = "Path to the file")]
     path: std::path::PathBuf,
+    #[clap(short, long, help = "Print raw digest")]
+    unformatted: bool,
 }
 
 fn main() {
     let args = Cli::parse();
 
-    let content = std::fs::read(&args.path).unwrap();
+    let file = std::fs::read(&args.path);
+    let file = match file {
+        Ok(ct) => ct,
+        Err(error) => match error.kind() {
+            std::io::ErrorKind::NotFound => {
+                eprintln!("{}{} cannot find the file specified", "error".red().bold(), ":".bold());
+                std::process::exit(2)
+            },
+            other_error => panic!("{:?}", other_error)
+        }
+    };
+
     let result = match args.algorithm.as_str() { 
-        "md5" => util::compute_md5(content),
-        "sha128" => util::compute_sha128(content),
-        "sha224" => util::compute_sha224(content),
-        "sha256" => util::compute_sha256(content),
-        "sha384" => util::compute_sha384(content),
-        "sha512" => util::compute_sha512(content),
+        "md5" => util::compute_md5(file),
+        "sha128" => util::compute_sha128(file),
+        "sha224" => util::compute_sha224(file),
+        "sha256" => util::compute_sha256(file),
+        "sha384" => util::compute_sha384(file),
+        "sha512" => util::compute_sha512(file),
         
         // It's impossible to get to this arm. But if you do, please make an issue
         _ => panic!("unknown algorithm {:?}", args.algorithm)
     };
 
-    println!("The {} hash of {:?} is {}", args.algorithm, args.path, result)
+    let output = if args.unformatted {
+        format!("{}", result)
+    } else {
+        format!("The {} hash of {:?} is {}", args.algorithm.to_ascii_uppercase(), args.path, result)
+    };
+    println!("{}", output)
 }
